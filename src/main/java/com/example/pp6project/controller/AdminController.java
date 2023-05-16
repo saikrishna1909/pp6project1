c57215538ae9e154d2a58ab570661aeed9dc9e63
N SAIKRISHNA REDDY <saikrishnareddy.nagulapally2@gmail.com>
2023-05-17 02:43:56 +0530

    mycommit

--- src/main/java/com/example/pp6project/controller/AdminController.java
+++ src/main/java/com/example/pp6project/controller/AdminController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.pp6project.Entity.Admin;
 import com.example.pp6project.Repository.AdminRepository;
@@ -21,9 +23,14 @@ import com.example.pp6project.Repository.AdminRepository;
 public class AdminController {
 	@Autowired
 	public AdminRepository a_repo;
+	@Autowired
+	 private Environment env;
 	@RequestMapping("/welocome")
-	public String welcomePage() {
-		return "adminlogin";
+	public ModelAndView welcomePage() {
+		var admin = new ModelAndView();
+	    admin.setViewName("adminlogin");
+		 
+		return admin;
 	}
 	@GetMapping("/adminvalidation")
 	public String adminPage(@RequestParam("email") String email, @RequestParam("password") String password,
